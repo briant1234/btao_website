07923903b57f751d9c3409d86d2449111f2efa46
BrianTao <btao.person@gmail.com>
2021-04-01 23:07:08 -0400

    remove base web, and add material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
-import { Client as Styletron } from "styletron-engine-atomic";
-import { Provider as StyletronProvider } from "styletron-react";
-import { LightTheme, BaseProvider } from "baseui";
-import { Header } from "./Components";
 
-const engine = new Styletron();
+import { Icon } from "@material-ui/core"
+
 
 function App() {
   return (
     <div className="App">
-      <StyletronProvider value={engine}>
-        <BaseProvider theme={LightTheme}>
-          <Header />
-        </BaseProvider>
-      </StyletronProvider>
+          <p>hello world</p>
+        <Icon>close</Icon>
     </div>
   );
 }
