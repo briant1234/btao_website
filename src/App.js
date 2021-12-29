7cffc036fc14223fa300c19b150d8d977002e799
BrianTao <btao.person@gmail.com>
2021-12-29 11:18:26 -0500

    switch to hash router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //React Router
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //Import theme
 import { ThemeProvider } from "@material-ui/core/styles";
